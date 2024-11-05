ee08b2adfbb00a1ba64b35578a60dd792173a927
ahmedabougabal <ahmedabougabal@live.com>
2024-11-05 21:41:38 +0200

    doc: Adds better comments for clarification of how the algo works!

--- InsertionSortAlgorithm.py
+++ InsertionSortAlgorithm.py
@@ -2,21 +2,19 @@
 Insertion Sort Algorithm Implementation
 """
 
+
 def insertionSort(array):
     for i in range(1, len(array)):
         current = array[i]
         position = i - 1
-        while position >= 0 and current < array[position]:
+        while position >= 0 and current < array[position]:  # while loop shifts the elements in the sorted portion
+            # (elements on the left of the current index) to the right to make room for the coming element (from the right side)'(unsorted portion)'
             array[position + 1] = array[position]
             position -= 1
-        
-        # then i have to insert current element i am on to the correct position 
+
+        # then I have to insert current element i am on to the correct position
         array[position + 1] = current
     return array
-
-
-
-
 
 
 arr = [12, 43, 65, 3, 65, 7, 45, 2, 3, 61, 0, 1]
