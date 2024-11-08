89b5463337f9b3c6712e75c9fd398015cda1c7eb
ahmedabougabal <ahmedabougabal@live.com>
2024-11-08 21:04:14 +0200

    Feat: Adds another offset config for directions which gave another correct path to the goal coordinates

--- mazeCodingChallenge_BFS/helpers.py
+++ mazeCodingChallenge_BFS/helpers.py
@@ -5,6 +5,16 @@ offsets = {
   "down" : (1,0),
 }
 
+'''
+this offset config works well too
+offsets = {
+  "up" : (-1,0),
+  "down": (1,0),
+  "left" : (0,-1),
+  "right":(0,1)
+}
+
+'''
 
 def create_maze():
     return [
